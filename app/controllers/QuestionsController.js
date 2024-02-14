a25d559e11232ef070fe57e6bb55b7e79aa678c1
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-13 17:14:42 -0700

    set up service, calling get in constructor

--- app/controllers/QuestionsController.js
+++ app/controllers/QuestionsController.js
@@ -1,5 +1,16 @@
+import { questionsService } from "../services/QuestionsService.js";
+import { Pop } from "../utils/Pop.js";
+
 export class QuestionsController {
   constructor () {
-    console.log('Questions controller loaded');
+    this.getQuestions()
+  }
+  async getQuestions() {
+    try {
+      await questionsService.getQuestions()
+    } catch (error) {
+      console.error(error);
+      Pop.error(error)
+    }
   }
 }
---
+++ app/services/QuestionsService.js
@@ -0,0 +1,8 @@
+class QuestionsService {
+  getQuestions() {
+    console.log('getting questions');
+  }
+
+}
+
+export const questionsService = new QuestionsService()
